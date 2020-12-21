7a7a7de5ec361c1606cfd3f73b4ad8c896a524b7
Eduard Filip <eduard.filip@secrethub.io>
2020-12-21 09:43:33 +0200

    Adjust demo to the new version of the cobra cli

--- cli/serve.go
+++ cli/serve.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/secrethub/demo-app/app"
 
+	"github.com/secrethub/secrethub-cli/internals/cli"
 	"github.com/secrethub/secrethub-cli/internals/cli/ui"
-	"github.com/secrethub/secrethub-cli/internals/secrethub/command"
 )
 
 type ServeCommand struct {
@@ -25,14 +25,14 @@ func NewServeCommand(io ui.IO) *ServeCommand {
 }
 
 // Register registers the command, arguments and flags on the provided Registerer.
-func (cmd *ServeCommand) Register(r command.Registerer) {
-	clause := r.CreateCommand("serve", "Runs the secrethub example by serving a web page.")
+func (cmd *ServeCommand) Register(r cli.Registerer) {
+	clause := r.Command("serve", "Runs the secrethub example by serving a web page.")
 
-	clause.StringVarP(&cmd.host, "host", "h", "127.0.0.1", "The host to serve the webpage on", true, false)
-	clause.IntVar(&cmd.port, "port", 8080, "The port to serve the webpage on", true, false)
-	clause.StringVar(&cmd.altPage, "alt-page", "", "Path to alternative page file to serve", false, false)
+	clause.Flags().StringVarP(&cmd.host, "host", "h", "127.0.0.1", "The host to serve the webpage on")
+	clause.Flags().IntVar(&cmd.port, "port", 8080, "The port to serve the webpage on")
+	clause.Flags().StringVar(&cmd.altPage, "alt-page", "", "Path to alternative page file to serve")
 
-	command.BindAction(clause, nil, cmd.Run)
+	clause.BindAction(cmd.Run)
 }
 
 // Run handles the command with the options as specified in the command.
